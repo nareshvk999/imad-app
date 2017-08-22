ac1bb0dfebf3c10cd37107efe086da7744417f87
nareshvk999 <31017066+nareshvk999@users.noreply.github.com>
2017-08-22 16:57:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res) {
-    res.sendfile('article one requested and will be served here');
+    res.send('article one requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
